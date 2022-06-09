e011753c36373c5c09a85a31357841714c8ece2c
FabianLamas <fabian.lamas2.0@gmail.com>
2022-06-09 13:58:05 -0300

    First Commit

---
+++ Test-Geminus/GeminusEx-Two/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeminusEx_Two
+{
+    internal class Program
+    {
+        private static int areaLength = 4;
+        private static int[] myArray = new int[] { 1, 2, -1, 1, 0, 1, 2, -1, -1, -2 };
+
+        public static void Main(string[] args)
+        {
+            List<int> horizontal = new List<int>();
+            List<int> vertical = new List<int>();
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    horizontal.Add(myArray[i]);
+                }
+
+                if (i % 2 != 0)
+                {
+                    vertical.Add(myArray[i]);
+                }
+            }
+
+            int finalX = CalculateMoves(horizontal);
+            int finalY = CalculateMoves(vertical);
+
+            PrintArea(finalX, finalY);
+            
+        }
+
+        public static void PrintArea(int x, int y)
+        {
+            string[,] matriz = new string[areaLength, areaLength];
+
+            for (int i = 0; i < areaLength; i++)
+            {
+                for (int j = 0; j < areaLength; j++)
+                {
+                    matriz[i, j] = "O";
+                }
+            }
+
+            matriz[y,x] = "X";
+
+            for (int i = 0; i < areaLength; i++)
+            {
+                for (int j = 0; j < areaLength; j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int CalculateMoves(List<int> values)
+        {
+            int position = 0;
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (position + values[i] >= 0 && position + values[i] <= 3)
+                {
+                    position += values[i];
+                }
+            }
+
+            return position;
+        }
+    }
+}
---
+++ Test-Geminus/Test-Geminus/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Test_Geminus
+{
+    internal class Program
+    {
+        private static int bottom = 1;
+        private static int top = 10;
+        private static int defaultValue = 5;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Case 6");
+            PrintBoard(6);
+        }
+
+        public static void PrintBoard(int inputN)
+        {
+            int squares = GetNumberOfSquares(inputN);
+            String[,] board = new String[squares, squares];
+
+            for (int i = 0; i < squares; i++)
+            {
+                for (int j = 0; j < squares; j++)
+                {
+                    board[i,j] = (i + j) % 2 == 0 ? "X" : "_";
+                }
+            }
+
+            for (int i = 0; i < squares; i++)
+            {
+                for (int j = 0; j < squares; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int GetNumberOfSquares(int numberOfSquares)
+        {
+            return TestRange(numberOfSquares, bottom, top) ? numberOfSquares : defaultValue;
+        }
+
+        private static bool TestRange(int numberToCheck, int bottom, int top)
+        {
+            return (numberToCheck >= bottom && numberToCheck <= top);
+        }
+    }
+}
